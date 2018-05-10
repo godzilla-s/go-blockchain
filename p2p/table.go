248364ca73cc7cb3154bcefbe895923b6c874f7e
Weijin Zhu <472214158@qq.com>
2018-05-10 23:29:11 +0800

    udpdate

--- p2p/table.go
+++ p2p/table.go
@@ -6,24 +6,79 @@ import (
 
 type nodeDB struct {
 	mux   sync.Mutex
-	cache map[string]*Node
+	cache map[string]nodeCache
+}
+
+type nodeCache struct {
+	node     *Node
+	lastPing int64
+	fails    int
 }
 
 func newNodeDB() *nodeDB {
 	return &nodeDB{
-		cache: make(map[string]*Node),
+		cache: make(map[string]nodeCache),
 	}
 }
 
-func (db *nodeDB) save(n *Node) {
+func (db *nodeDB) saveNode(n *Node) {
 	db.mux.Lock()
 	defer db.mux.Unlock()
 
-	db.cache[n.ID] = n
+	db.cache[n.ID] = nodeCache{node: n}
 }
 
-func (db *nodeDB) updateLastTime(id string, now int64) {
+func (db *nodeDB) findFails(id string) int {
+	db.mux.Lock()
+	defer db.mux.Unlock()
 
+	nc, ok := db.cache[id]
+	if !ok {
+		return 0
+	}
+	return nc.fails
+}
+
+func (db *nodeDB) updateFails(id string, fails int) {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	nc, ok := db.cache[id]
+	if !ok {
+		return
+	}
+	nc.fails = fails
+	db.cache[id] = nc
+}
+
+func (db *nodeDB) deleteNode(id string) {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	delete(db.cache, id)
+}
+
+func (db *nodeDB) lastPing(id string) int64 {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	nc, ok := db.cache[id]
+	if !ok {
+		return 0
+	}
+	return nc.lastPing
+}
+
+func (db *nodeDB) updateLastPing(id string, now int64) {
+	db.mux.Lock()
+	defer db.mux.Unlock()
+
+	nc, ok := db.cache[id]
+	if !ok {
+		return
+	}
+	nc.lastPing = now
+	db.cache[id] = nc
 }
 
 type Table struct {
@@ -40,4 +95,8 @@ func (tab *Table) explore() {
 	for {
 
 	}
+}
+
+func (tab *Table) bondNode(n *Node) {
+
 }
